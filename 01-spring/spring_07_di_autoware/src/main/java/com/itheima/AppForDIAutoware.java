18a2bfb428ab1605be8144f80ef5160f8c8577a9
Jxiang2 <xjyhenry@gmail.com>
2022-07-02 14:28:42 -0400

    bean done

--- 01-spring/spring_07_di_autoware/src/main/java/com/itheima/AppForDIAutoware.java
+++ 01-spring/spring_07_di_autoware/src/main/java/com/itheima/AppForDIAutoware.java
@@ -1,6 +1,5 @@
 package com.itheima;
 
-import com.itheima.dao.BookDao;
 import com.itheima.service.BookService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
