7b2c8e931b163cf00a851fc8ffc4dae4b3001be2
Jxiang2 <xjyhenry@gmail.com>
2022-07-02 09:32:34 -0400

    bean done

--- 01-spring/spring_04_bean_lifecycle/src/main/java/com/itheima/AppForLifeCycle.java
+++ 01-spring/spring_04_bean_lifecycle/src/main/java/com/itheima/AppForLifeCycle.java
@@ -1,7 +1,6 @@
 package com.itheima;
 
 import com.itheima.dao.BookDao;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class AppForLifeCycle {
