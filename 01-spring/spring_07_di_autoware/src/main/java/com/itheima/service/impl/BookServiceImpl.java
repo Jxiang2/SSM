5b0583909c76443e5f350fc743761d2b696ab202
Jxiang2 <xjyhenry@gmail.com>
2022-07-03 12:01:15 -0400

    config remove btn and icon

--- 01-spring/spring_07_di_autoware/src/main/java/com/itheima/service/impl/BookServiceImpl.java
+++ 01-spring/spring_07_di_autoware/src/main/java/com/itheima/service/impl/BookServiceImpl.java
@@ -2,10 +2,12 @@ package com.itheima.service.impl;
 
 import com.itheima.dao.BookDao;
 import com.itheima.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class BookServiceImpl implements BookService{
     private BookDao bookDao;
 
+    @Autowired
     public void setBookDao(BookDao bookDao) {
         this.bookDao = bookDao;
     }
