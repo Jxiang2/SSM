200dbcd110d22e320a4a6796d67fc52d72fc2724
Jxiang2 <xjyhenry@gmail.com>
2022-07-17 16:15:56 -0400

    refine ui done

--- 01-spring/spring_23_case_handle_password/src/main/java/com/itheima/aop/DataAdvice.java
+++ 01-spring/spring_23_case_handle_password/src/main/java/com/itheima/aop/DataAdvice.java
@@ -17,6 +17,7 @@ public class DataAdvice {
     @Around("DataAdvice.servicePt()")
     public Object trimStr(ProceedingJoinPoint pjp) throws Throwable {
         Object[] args = pjp.getArgs();
+        System.out.println(Arrays.toString(args));
 
         for (int i = 0; i < args.length; i++) {
             //判断参数是不是字符串
