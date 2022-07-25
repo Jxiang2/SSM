0a9c18da2efb8217eabe010e9e037ca6fc99c367
Jxiang2 <xjyhenry@gmail.com>
2022-07-25 17:23:02 -0400

    update spring mvc

--- 02-springmvc/springmvc_01_quickstart/src/main/java/com/itheima/config/ServletContainersInitConfig.java
+++ 02-springmvc/springmvc_01_quickstart/src/main/java/com/itheima/config/ServletContainersInitConfig.java
@@ -8,16 +8,14 @@ import org.springframework.web.servlet.support.AbstractDispatcherServletInitiali
 public class ServletContainersInitConfig extends AbstractDispatcherServletInitializer {
     //加载springmvc配置类，产生springmvc容器（本质还是spring容器）
     protected WebApplicationContext createServletApplicationContext() {
-        //初始化WebApplicationContext对象
-        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
-        //加载指定配置类
-        ctx.register(SpringMvcConfig.class);
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext(); //初始化WebApplicationContext对象
+        ctx.register(SpringMvcConfig.class); //加载指定配置类
         return ctx;
     }
 
     //设置由springmvc控制器处理的请求映射路径
     protected String[] getServletMappings() {
-        return new String[]{"/"};
+        return new String[]{"/",};
     }
 
     //加载spring配置类
--- 02-springmvc/springmvc_01_quickstart/src/main/java/com/itheima/controller/UserController.java
+++ 02-springmvc/springmvc_01_quickstart/src/main/java/com/itheima/controller/UserController.java
@@ -7,21 +7,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 //定义表现层控制器bean
 @Controller
 public class UserController {
-
-    //设置映射路径为/save，即外部访问路径
-    @RequestMapping("/save")
-    //设置当前操作返回结果为指定json数据（本质上是一个字符串信息）
-    @ResponseBody
+    @RequestMapping("/save") //设置映射路径为/save，即外部访问路径
+    @ResponseBody //设置当前操作返回结果为指定json数据（本质上是一个字符串信息）
     public String save(){
         System.out.println("user save ...");
-        return "{'info':'springmvc'}";
+        return "{'info':'springmvc save'}";
     }
 
-    //设置映射路径为/delete，即外部访问路径
-    @RequestMapping("/delete")
+    @RequestMapping("/delete") //设置映射路径为/delete，即外部访问路径
     @ResponseBody
     public String delete(){
-        System.out.println("user save ...");
-        return "{'info':'springmvc'}";
+        System.out.println("user delete ...");
+        return "{'info':'springmvc delete'}";
     }
 }
